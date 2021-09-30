2edbd74be58700b652b19f69d669210437ab9e31
cauanasenna <cauanasenna@gmail.com>
2021-09-29 23:53:53 -0300

    função quemganhou() funcionando

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let jogadorEscolha = ''
 let computadorEscolha = ''
-let ganhador = 0
+let computadorNome = ''
+let ganhador = ''
 let jogadorPontuacao = 0
 let computadorPontuacao = 0
 
@@ -8,13 +9,18 @@ const botoes = document.querySelectorAll('button')
 console.log(botoes)
 botoes.forEach((button) => button.addEventListener('click', function (evt) { 
     jogadorEscolha = evt.target.id
-    console.log(jogadorEscolha)
-    console.log(computadorEscolhaNome())
-    console.log(quemGanhou())
-    
+    console.log('jogador escolheu: ' + jogadorEscolha)
+    console.log('jogada do computador: ' + computadorEscolhaNome())
+    computadorEscolhaNome()
+    console.log('nome jogada do computador: ' + computadorNome)
+    jogar()
+    console.log('nome ganhador: ' + ganhador)
+    quemGanhou()
+    console.log('quem ganhou: ' + quemGanhou())
+
     computadorEscolha = evt.target.id
     ganhador = evt.target.id
-    quemGanhou()
+    
 }))
 
 //button.addEventListener('click', function main() { // estou chamando a função main que vai acontecer tudo o que coloquei embaixo quando eu der um clique no botão
@@ -26,16 +32,16 @@ function computadorEscolhaNome() {
     computadorEscolha = Math.floor((Math.random() * 3) + 1)
     let showComputer = document.getElementById('resultado-computador')
     if (computadorEscolha === 1) {
-        computadorEscolha = "pedra";
+        computadorNome = "pedra";
         showComputer.innerText ='Pedra'
     } else if (computadorEscolha === 2) {
-        computadorEscolha = "papel"
+        computadorNome = "papel"
         showComputer.innerText ='Papel'
     } else if (computadorEscolha === 3) {
-        computadorEscolha = "tesoura"
+        computadorNome = "tesoura"
         showComputer.innerText ='Tesoura'
     }
-    return computadorEscolha
+    return computadorNome
 }
 
 // definir critérios do jogo e quem irá ganhar
@@ -43,23 +49,23 @@ function computadorEscolhaNome() {
 function jogar() {
     console.log('função jogar ' + jogadorEscolha)
 
-    if (jogadorEscolha === "papel" && computadorEscolha === "papel") {
+    if (jogadorEscolha === "papel" && computadorNome === "papel") {
         ganhador = "empate";
-    } else if (jogadorEscolha === "papel" && computadorEscolha === "pedra") {
+    } else if (jogadorEscolha === "papel" && computadorNome === "pedra") {
         ganhador = "jogador";
-    } else if (jogadorEscolha === "papel" && computadorEscolha === "tesoura") {
+    } else if (jogadorEscolha === "papel" && computadorNome === "tesoura") {
         ganhador = "computador";
-    } else if (jogadorEscolha === "pedra" && computadorEscolha === "pedra") {
+    } else if (jogadorEscolha === "pedra" && computadorNome === "pedra") {
         ganhador = "empate";
-    } else if (jogadorEscolha === "pedra" && computadorEscolha === "papel") {
+    } else if (jogadorEscolha === "pedra" && computadorNome === "papel") {
         ganhador = "computador";
-    } else if (jogadorEscolha === "pedra" && computadorEscolha === "tesoura") {
+    } else if (jogadorEscolha === "pedra" && computadorNome === "tesoura") {
         ganhador = "jogador";
-    } else if (jogadorEscolha === "tesoura" && computadorEscolha === "tesoura") {
+    } else if (jogadorEscolha === "tesoura" && computadorNome === "tesoura") {
         ganhador = "empate";
-    } else if (jogadorEscolha === "tesoura" && computadorEscolha === "papel") {
+    } else if (jogadorEscolha === "tesoura" && computadorNome === "papel") {
         ganhador = "jogador";
-    } else if (jogadorEscolha === "tesoura" && computadorEscolha === "pedra") {
+    } else if (jogadorEscolha === "tesoura" && computadorNome === "pedra") {
         ganhador = "computador";
     }
     return ganhador
@@ -71,16 +77,16 @@ function quemGanhou() {
 
     let showWinner =  document.getElementById('resultado-centro-winner')
 
-    if (ganhador = "jogador") {
+    if (ganhador === "jogador") {
         showWinner.innerText = 'Parabéns! Você ganhou'
         jogadorPontuacao++
-    } else if (ganhador = "empate") {
+    } else if (ganhador === "empate") {
         showWinner.innerText ='Empate!'
-    } else if (ganhador = "computador") {
+    } else if (ganhador === "computador") {
         showWinner.innerText = 'Ah que pena, você perdeu :('
         computadorPontuacao++
     }
-    return ganhador
+    return showWinner
 }
 // somar pontos
 
