6ae35dd0ea3da2970061cb172bfa1e571c58c00b
cauanasenna <cauanasenna@gmail.com>
2021-09-29 22:37:42 -0300

    mudanças na estrutura de quem ganhou

--- script.js
+++ script.js
@@ -1,35 +1,47 @@
-const botoes = document.querySelectorAll('button')
-botoes.forEach((button) => button.addEventListener('click', (e) => console.log(e.target.id)))
-
-addEventListener('click', function main() {
-
-let jogadorEscolha = 0
-let computadorEscolha = 0
+let jogadorEscolha = ''
+let computadorEscolha = ''
 let ganhador = 0
 let jogadorPontuacao = 0
 let computadorPontuacao = 0
 
+const botoes = document.querySelectorAll('button')
+console.log(botoes)
+botoes.forEach((button) => button.addEventListener('click', function (evt) { 
+    jogadorEscolha = evt.target.id
+    console.log(jogadorEscolha)
+    console.log(computadorEscolhaNome())
+    console.log(quemGanhou())
+    
+    computadorEscolha = evt.target.id
+    ganhador = evt.target.id
+    quemGanhou()
+}))
+
+//button.addEventListener('click', function main() { // estou chamando a função main que vai acontecer tudo o que coloquei embaixo quando eu der um clique no botão
+
+
 // definir jogadas do computador
 
 function computadorEscolhaNome() {
-    computadorEscolha = Math.floor((Math.random () *3) +1)
+    computadorEscolha = Math.floor((Math.random() * 3) + 1)
+    let showComputer = document.getElementById('resultado-computador')
     if (computadorEscolha === 1) {
         computadorEscolha = "pedra";
-        document.getElementById('resultado-computador').innerText('Pedra')
-    } else if (computadorEscolha === 2){
+        showComputer.innerText ='Pedra'
+    } else if (computadorEscolha === 2) {
         computadorEscolha = "papel"
-        document.getElementById('resultado-computador').innerText('Papel')
+        showComputer.innerText ='Papel'
     } else if (computadorEscolha === 3) {
         computadorEscolha = "tesoura"
-        document.getElementByIdById('resultado-computador').innerText('Tesoura')
+        showComputer.innerText ='Tesoura'
     }
     return computadorEscolha
 }
 
 // definir critérios do jogo e quem irá ganhar
 
-function jogar(escolha) {
-    jogadorEscolha = escolha
+function jogar() {
+    console.log('função jogar ' + jogadorEscolha)
 
     if (jogadorEscolha === "papel" && computadorEscolha === "papel") {
         ganhador = "empate";
@@ -50,28 +62,31 @@ function jogar(escolha) {
     } else if (jogadorEscolha === "tesoura" && computadorEscolha === "pedra") {
         ganhador = "computador";
     }
-    return escolha
+    return ganhador
 }
 
 // aparecer mensagem de quem ganhou
 
 function quemGanhou() {
 
+    let showWinner =  document.getElementById('resultado-centro-winner')
+
     if (ganhador = "jogador") {
-        document.getElementById('resultado').innerText('Parabéns! Você ganhour')
+        showWinner.innerText = 'Parabéns! Você ganhou'
         jogadorPontuacao++
     } else if (ganhador = "empate") {
-        document.getElementById('resultado').innerText('Empate!')
+        showWinner.innerText ='Empate!'
     } else if (ganhador = "computador") {
-        document.getElementById('resultado').innerText('Ah que pena, você perdeu :(')
+        showWinner.innerText = 'Ah que pena, você perdeu :('
         computadorPontuacao++
     }
+    return ganhador
 }
 // somar pontos
 
-        document.getElementById('pontos-player').innerHTML = jogadorPontuacao 
-        document.getElementById('pontos-computador').innerHTML = computadorPontuacao 
+document.getElementById('pontos-player').innerHTML = jogadorPontuacao
+document.getElementById('pontos-computador').innerHTML = computadorPontuacao
 
-return ganhador
 
-}) //fim da funçao main
+
+//}) //fim da funçao main
